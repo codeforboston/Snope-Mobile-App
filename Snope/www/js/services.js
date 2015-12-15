bd8855392bba3c700bdb689c204f81bb1ea39fdd
Christopher Michael Buck <butterwell@gmail.com>
2015-12-15 09:27:27 -0500

    Localstorage service (to persist email address)

--- Snope/www/js/services.js
+++ Snope/www/js/services.js
@@ -95,4 +95,21 @@ angular.module('starter.services', [])
       return q.promise;
     }
   }
+}])
+
+.factory('Localstorage', ['$window', function($window) {
+  return {
+    set: function(key, value) {
+      $window.localStorage[key] = value;
+    },
+    get: function(key, defaultValue) {
+      return $window.localStorage[key] || defaultValue;
+    },
+    setObject: function(key, value) {
+      $window.localStorage[key] = JSON.stringify(value);
+    },
+    getObject: function(key) {
+      return JSON.parse($window.localStorage[key] || '{}');
+    }
+  }
 }]);
