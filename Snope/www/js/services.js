6eb126e3a692134f9335ddc83806a2bbae972dc0
Christopher Michael Buck <butterwell@gmail.com>
2015-12-15 09:59:22 -0500

    simplify Localstorage service

--- Snope/www/js/services.js
+++ Snope/www/js/services.js
@@ -104,12 +104,6 @@ angular.module('starter.services', [])
     },
     get: function(key, defaultValue) {
       return $window.localStorage[key] || defaultValue;
-    },
-    setObject: function(key, value) {
-      $window.localStorage[key] = JSON.stringify(value);
-    },
-    getObject: function(key) {
-      return JSON.parse($window.localStorage[key] || '{}');
     }
   }
 }]);
